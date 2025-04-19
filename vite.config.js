b80ab4ec6e25368b398f50c90fe6293339e75520
Jonas Abarca <jonasabarca@MacBook-Air-de-Jonas.local>
2025-04-19 17:21:41 -0500

    Glob Sync

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import * as glob from 'glob'; // Import glob correctly
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                ...glob.sync('resources/css/*'),
+                ...glob.sync('resources/js/*'),
+                ...glob.sync('resources/img/*.png'),
+                ...glob.sync('resources/img/*.jpg'),
+                ...glob.sync('resources/img/*.ico'),
+                ...glob.sync('resources/img/*.svg'),
+                ...glob.sync('resources/img/screens/*.png'),
+            ],
             refresh: true,
         }),
     ],
